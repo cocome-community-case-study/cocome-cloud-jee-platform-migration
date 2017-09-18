cd0a92755819eaaf934d6a9f2f9a424dc4a5ec02
Rudolf Biczok <rcbiczok@gmail.com>
2017-09-18 10:00:40 +0200

    Remove redundant qualifiers and fix javadoc references

--- cocome-maven-project/cloud-logic-service/cloud-logic-core-api/src/main/java/org/cocome/tradingsystem/inventory/data/store/IStoreQuery.java
+++ cocome-maven-project/cloud-logic-service/cloud-logic-core-api/src/main/java/org/cocome/tradingsystem/inventory/data/store/IStoreQuery.java
@@ -20,6 +20,8 @@ import java.util.Collection;
 
 import javax.ejb.Local;
 
+import org.cocome.tradingsystem.inventory.application.store.IStoreInventoryLocal;
+import org.cocome.tradingsystem.inventory.application.store.IStoreInventoryManagerLocal;
 import org.cocome.tradingsystem.inventory.data.enterprise.IProduct;
 import org.cocome.tradingsystem.util.exception.NotInDatabaseException;
 
@@ -39,7 +41,7 @@ public interface IStoreQuery {
 	//
 
 	/**
-	 * Queries the database for a {@link Store} entity with given name and location.
+	 * Queries the database for a {@link IStore} entity with given name and location.
 	 * If more than one entity match these criteria, the first match will be returned.
 	 * 
 	 * @param name
@@ -48,81 +50,81 @@ public interface IStoreQuery {
 	 *            store location, may contain wildcard characters and may be empty.
 	 *            
 	 * @return
-	 *         {@link Store} entity or {@code null} if there is no store with
+	 *         {@link IStore} entity or {@code null} if there is no store with
 	 *         the given name and location.
 	 * 
 	 */
-	public IStore queryStore(String name, String location);
+	IStore queryStore(String name, String location);
 
 	/**
-	 * Returns a {@link Store} entity corresponding to the given unique identifier.
+	 * Returns a {@link IStore} entity corresponding to the given unique identifier.
 	 * 
 	 * @param storeId
-	 *            unique identifier of a {@link Store} entity
+	 *            unique identifier of a {@link IStore} entity
 	 *            
 	 * @return
-	 *         {@link Store} entity
+	 *         {@link IStore} entity
 	 *         
 	 * @throws NotInDatabaseException 
 	 *             if a store with the given id could not be found 
 	 */
-	public IStore queryStoreById(long storeId) throws NotInDatabaseException;
+	IStore queryStoreById(long storeId) throws NotInDatabaseException;
 
 	/**
-	 * Returns a {@link StockItem} entity corresponding to the given unique identifier.
+	 * Returns a {@link IStockItem} entity corresponding to the given unique identifier.
 	 * 
 	 * @param stockItemId
-	 *            unique identifier of a {@link StockItem} entity
+	 *            unique identifier of a {@link IStockItem} entity
 	 *            
 	 * @return
-	 *         {@link StockItem} entity
+	 *         {@link IStockItem} entity
 	 * 
 	 * @throws NotInDatabaseException 
 	 *             if a stock item with the given id could not be found
 	 */
-	public IStockItem queryStockItemById(long stockItemId) throws NotInDatabaseException ;
+	IStockItem queryStockItemById(long stockItemId) throws NotInDatabaseException ;
 
 	/**
-	 * Returns a {@link Product} entity corresponding to the given unique identifier.
+	 * Returns a {@link IProduct} entity corresponding to the given unique identifier.
 	 * 
 	 * @param productId
-	 *            unique identifier of a {@link Product} entity
+	 *            unique identifier of a {@link IProduct} entity
 	 *            
 	 * @return
-	 *         {@link Product} entity
+	 *         {@link IProduct} entity
 	 * 
 	 * @throws NotInDatabaseException 
 	 *             if a product with the given id could not be found
 	 */
-	public IProduct queryProductById(long productId) throws NotInDatabaseException ;
+	IProduct queryProductById(long productId) throws NotInDatabaseException ;
 
 	/**
-	 * Queries the database for a {@link Product} entity with given barcode.
+	 * Queries the database for a {@link IProduct} entity with given barcode.
 	 * 
 	 * @param barcode
 	 *            barcode of the product to find
 	 *            
 	 * @return
-	 *         {@link Product} entity with the specified barcode, or {@code null} if there is no such product.
+	 *         {@link IProduct} entity with the specified barcode, or {@code null} if there is no such product.
 	 */
-	public IProduct queryProductByBarcode(long barcode);
+	IProduct queryProductByBarcode(long barcode);
 
 	/**
-	 * Queries the database for a {@link ProductOrder} with given identifier.
+	 * Queries the database for a {@link IProductOrder} with given identifier.
 	 * <p>
 	 * The following methods from StoreIf use this method: List&gt;ComplexOrderTO&lt; orderProducts(ComplexOrderTO complexOrder, StoreTO storeTO); ComplexOrderTO
 	 * getOrder(int orderId); void rollInReceivedOrder(ComplexOrderTO complexOrder, StoreTO store);
 	 * 
 	 * @param orderId
-	 *            unique identifier of a {@link ProductOrder} entity
+	 *            unique identifier of a {@link IProductOrder} entity
 	 *            
 	 * @return
-	 *         An instance of {@link ProductOrder} with the specified id.
+	 *         An instance of {@link IProductOrder} with the specified id.
 	 * 
 	 * @throws NotInDatabaseException 
 	 *             if a product order with the given id could not be found
 	 */
-	public IProductOrder queryOrderById(long orderId) throws NotInDatabaseException ;
+	IProductOrder queryOrderById(long orderId) throws NotInDatabaseException ;
 
 	//
 	// Queries limited to a given store.
@@ -133,41 +135,41 @@ public interface IStoreQuery {
 	 * products for which there is a stock item.
 	 * 
 	 * @param storeId
-	 *            unique identifier of a {@link Store} entity
+	 *            unique identifier of a {@link IStore} entity
 	 *            
 	 * @return
 	 *         A collection of products carried by the given store.
 	 */
-	public Collection<IProduct> queryProducts(long storeId);
+	Collection<IProduct> queryProducts(long storeId);
 
 	/**
 	 * Queries the specified store for all outstanding product orders, i.e.
 	 * product orders which do not have their delivery date set.
 	 * 
 	 * @param storeId
-	 *            unique identifier of a {@link Store} entity
+	 *            unique identifier of a {@link IStore} entity
 	 *            
 	 * @return
 	 *         A collection of outstanding product orders in the specified store.
 	 */
-	public Collection<IProductOrder> queryOutstandingOrders(long storeId);
+	Collection<IProductOrder> queryOutstandingOrders(long storeId);
 	
 	/**
 	 * Queries the specified store for all product orders
 	 * 
 	 * @param storeId
-	 *            unique identifier of a {@link Store} entity
+	 *            unique identifier of a {@link IStore} entity
 	 *            
 	 * @return
 	 *         A collection of product orders in the specified store.
 	 */
-	public Collection<IProductOrder> queryAllOrders(long storeId);
+	Collection<IProductOrder> queryAllOrders(long storeId);
 	
 	/**
 	 * Queries the specified store for all product orders containing the given product.
 	 * 
 	 * @param storeId
-	 * 			unique identifier of a {@link Store} entity
+	 * 			unique identifier of a {@link IStore} entity
 	 *            
 	 * @param productBarcode
 	 * 			barcode of the required product
@@ -181,44 +183,44 @@ public interface IStoreQuery {
 	 * @throws NotInDatabaseException 
 	 * 			if no order with the given information can be found
 	 */
-	public IProductOrder queryProductOrder(long storeId, long productBarcode, long amount)  
+	IProductOrder queryProductOrder(long storeId, long productBarcode, long amount)
 			throws NotInDatabaseException ;
 
 	/**
 	 * Queries a store for all its stock items.
 	 * 
 	 * @param storeId
-	 *            unique identifier of a {@link Store} entity
+	 *            unique identifier of a {@link IStore} entity
 	 *            
 	 * @return
-	 *         A collection of {@link StockItem} entities.
+	 *         A collection of {@link IStockItem} entities.
 	 */
-	public Collection<IStockItem> queryAllStockItems(long storeId);
+	Collection<IStockItem> queryAllStockItems(long storeId);
 
 	/**
 	 * Queries a given store for all stock items with low stock.
 	 * 
 	 * @param storeId
-	 *            unique identifier of a {@link Store} entity
+	 *            unique identifier of a {@link IStore} entity
 	 * @return
-	 *         A collection of {@link StockItem} entities objects representing
+	 *         A collection of {@link IStockItem} entities objects representing
 	 *         low stock products in the given store.
 	 */
-	public Collection<IStockItem> queryLowStockItems(long storeId);
+	Collection<IStockItem> queryLowStockItems(long storeId);
 
 	/**
 	 * Queries the specified store for a stock item associated with product
 	 * identified by specified barcode.
 	 * 
 	 * @param storeId
-	 *            unique identifier of a {@link Store} entity
+	 *            unique identifier of a {@link IStore} entity
 	 * @param productBarcode
 	 *            product barcode for which to find the stock item
 	 * @return
 	 *         The StockItem from the given store for a product with the given
 	 *         barcode, or {@code null} if the stock item could not be found.
 	 */
-	public IStockItem queryStockItem(
+	IStockItem queryStockItem(
 			long storeId, long productBarcode
 			);
 
@@ -226,13 +228,13 @@ public interface IStoreQuery {
 	 * Returns the stock for the given productIds.
 	 * 
 	 * @param storeId
-	 *            unique identifier of a {@link Store} entity
+	 *            unique identifier of a {@link IStore} entity
 	 * @param productIds
-	 *            {@link Product} entity identifiers to look up in the stock
+	 *            {@link IProduct} entity identifiers to look up in the stock
 	 * @return
 	 *         The products as StockItems (including amounts)
 	 */
-	public Collection<IStockItem> queryStockItemsByProductId(
+	Collection<IStockItem> queryStockItemsByProductId(
 			long storeId, long[] productIds
 			);
 
