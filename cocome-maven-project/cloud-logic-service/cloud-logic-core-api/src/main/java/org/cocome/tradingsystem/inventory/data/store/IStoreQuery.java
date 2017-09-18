c6a14dc2dcbec666af0fc788184f51932e807566
Rudolf Biczok <rcbiczok@gmail.com>
2017-09-18 10:03:54 +0200

    Fix dangling javadoc

--- cocome-maven-project/cloud-logic-service/cloud-logic-core-api/src/main/java/org/cocome/tradingsystem/inventory/data/store/IStoreQuery.java
+++ cocome-maven-project/cloud-logic-service/cloud-logic-core-api/src/main/java/org/cocome/tradingsystem/inventory/data/store/IStoreQuery.java
@@ -1,4 +1,5 @@
-/***************************************************************************
+/*
+ *************************************************************************
  * Copyright 2013 DFG SPP 1593 (http://dfg-spp1593.de)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ***************************************************************************/
+ *************************************************************************
+ */
 
 package org.cocome.tradingsystem.inventory.data.store;
 
