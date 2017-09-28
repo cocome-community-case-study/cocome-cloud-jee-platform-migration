356789222ecee508caaf1d2376796bbd68d64df6
Rudolf Biczok <rcbiczok@gmail.com>
2017-09-28 22:43:03 +0200

    Rearrange methods

--- cocome-maven-project/cloud-logic-service/cloud-logic-core-services/src/main/java/org/cocome/logic/webservice/enterpriseservice/IEnterpriseManager.java
+++ cocome-maven-project/cloud-logic-service/cloud-logic-core-services/src/main/java/org/cocome/logic/webservice/enterpriseservice/IEnterpriseManager.java
@@ -38,6 +38,12 @@ import java.util.Collection;
  */
 @WebService(targetNamespace = "http://enterprise.webservice.logic.cocome.org/")
 public interface IEnterpriseManager {
+
+    /* CRUD for {@link EnterpriseTO} **************/
+
+    @WebMethod
+    Collection<EnterpriseTO> getEnterprises();
+
     /**
      * @return A EnterpriseTO object with the specified id.
      * @throws NotInDatabaseException if a trading enterprise with the given id could not be found
@@ -55,14 +61,17 @@ public interface IEnterpriseManager {
     EnterpriseTO queryEnterpriseByName(
             @XmlElement(required = true) @WebParam(name = "enterpriseName") String enterpriseName) throws NotInDatabaseException;
 
-    /**
-     * @param enterpriseId the unique identifier of a TradingEnterprise entity
-     * @return A collection of StoreWithEntepriseTO objects belonging to the given enterprise.
-     * @throws NotInDatabaseException if a trading enterprise with the given id could not be found
-     */
     @WebMethod
-    Collection<StoreWithEnterpriseTO> queryStoresByEnterpriseID(
-            @XmlElement(required = true) @WebParam(name = "enterpriseID") long enterpriseId) throws NotInDatabaseException;
+    long createEnterprise(
+            @XmlElement(required = true) @WebParam(name = "enterpriseName") String enterpriseName)
+            throws CreateException;
+
+    @WebMethod
+    void deleteEnterprise(
+            @XmlElement(required = true) @WebParam(name = "enterpriseTO") EnterpriseTO enterpriseTO)
+            throws NotInDatabaseException, UpdateException, IOException;
+
+    /* CRUD for {@link PlantTO} **************/
 
     /**
      * @param enterpriseId the unique identifier of a TradingEnterprise entity
@@ -74,19 +83,7 @@ public interface IEnterpriseManager {
             @XmlElement(required = true) @WebParam(name = "enterpriseID") long enterpriseId) throws NotInDatabaseException;
 
     /**
-     * @param enterpriseId the unique identifier of a TradingEnterprise entity
-     * @param storeId      the unique identifier of a Store entity
-     * @return A StoreWithEntepriseTO object with the given store identifier and
-     * belonging to the given enterprise.
-     * @throws NotInDatabaseException if a trading enterprise with the given id could not be found
-     */
-    @WebMethod
-    StoreWithEnterpriseTO queryStoreByEnterpriseID(
-            @XmlElement(required = true) @WebParam(name = "enterpriseID") long enterpriseId,
-            @XmlElement(required = true) @WebParam(name = "storeID") long storeId) throws NotInDatabaseException;
-
-    /**
-     * @param plantId      the unique identifier of a Plant entity
+     * @param plantId the unique identifier of a Plant entity
      * @return A PlantWithEntepriseTO object with the given store identifier and
      * belonging to the given enterprise.
      * @throws NotInDatabaseException if a trading enterprise with the given id could not be found
@@ -94,22 +91,6 @@ public interface IEnterpriseManager {
     @WebMethod
     PlantTO queryPlantByID(
             @XmlElement(required = true) @WebParam(name = "plantID") long plantId) throws NotInDatabaseException;
-
-    /**
-     * Queries the database for a store with the given name in the given enterprise.
-     * If there are multiple stores with the given name, all of them are returned.
-     * If there is no store with the given name, an empty collection is returned.
-     *
-     * @param enterpriseId the unique identifier of a TradingEnterprise entity
-     * @param storeName    the name of a Store entity
-     * @return All StoreWithEntepriseTO objects matching the given store name and
-     * belonging to the given enterprise.
-     * @throws NotInDatabaseException if a trading enterprise with the given id could not be found
-     */
-    @WebMethod
-    Collection<StoreWithEnterpriseTO> queryStoreByName(
-            @XmlElement(required = true) @WebParam(name = "enterpriseID") long enterpriseId,
-            @XmlElement(required = true) @WebParam(name = "storeName") String storeName) throws NotInDatabaseException;
 
     /**
      * Queries the database for a plant with the given name in the given enterprise.
@@ -127,16 +108,29 @@ public interface IEnterpriseManager {
             @XmlElement(required = true) @WebParam(name = "enterpriseID") long enterpriseId,
             @XmlElement(required = true) @WebParam(name = "PlantName") String plantName) throws NotInDatabaseException;
 
+    @WebMethod
+    long createPlant(
+            @XmlElement(required = true) @WebParam(name = "plantTO") PlantTO plantTO)
+            throws CreateException;
+
     /**
-     * @param supplier   The supplier which delivers the products
-     * @param enterprise The enterprise for which the products are delivered
-     * @return The mean time to delivery in milliseconds
+     * Updates the plant object. This method requires the EnterpriseTO to be present and to have
+     * at least the id attribute set.
+     *
+     * @param plantTO
+     * @throws UpdateException
      * @throws NotInDatabaseException
      */
     @WebMethod
-    long getMeanTimeToDelivery(
-            @XmlElement(required = true) @WebParam(name = "productSupplier") SupplierTO supplier,
-            @XmlElement(required = true) @WebParam(name = "enterprise") EnterpriseTO enterprise) throws NotInDatabaseException;
+    void updatePlant(
+            @XmlElement(required = true) @WebParam(name = "plantTO") PlantTO plantTO)
+            throws UpdateException, NotInDatabaseException;
+
+    @WebMethod
+    void deletePlant(
+            @XmlElement(required = true)
+            @WebParam(name = "plantTO") PlantTO plantTO)
+            throws NotInDatabaseException, UpdateException, IOException;
 
     /**
      * Retrieves all products that are sold in this enterprise.
@@ -152,6 +146,8 @@ public interface IEnterpriseManager {
             @XmlElement(required = true) @WebParam(name = "enterpriseID") long enterpriseId)
             throws NotInDatabaseException;
 
+    /* CRUD for {@link ProductTO} **************/
+
     /**
      * Retrieves all products that are registered in the database.
      * Note that there is no information included about the stores in which
@@ -161,30 +157,6 @@ public interface IEnterpriseManager {
      */
     @WebMethod
     Collection<ProductTO> getAllProducts();
-
-    /**
-     * Retrieves all products that are sold in this enterprise.
-     * Note that there is no information included about the stores in which
-     * this product is available.
-     *
-     * @param enterpriseId The enterprise for which all products should be retrieved
-     * @return All {@code ProductTO}s available in the given enterprise
-     * @throws NotInDatabaseException
-     */
-    @WebMethod
-    Collection<CustomProductTO> getAllEnterpriseCustomProducts(
-            @XmlElement(required = true) @WebParam(name = "enterpriseID") long enterpriseId)
-            throws NotInDatabaseException;
-
-    /**
-     * Retrieves all products that are registered in the database.
-     * Note that there is no information included about the stores in which
-     * these products are available.
-     *
-     * @return All {@code ProductTO}s available or an empty collection if there are none
-     */
-    @WebMethod
-    Collection<CustomProductTO> getAllCustomProducts();
 
     /**
      * Retrieves the product with the given ID if it is stored in the database.
@@ -210,6 +182,29 @@ public interface IEnterpriseManager {
     Collection<ProductTO> getProductsBySupplier(
             @XmlElement(required = true) @WebParam(name = "enterpriseID") long enterpriseID,
             @XmlElement(required = true) @WebParam(name = "supplierID") long supplierID) throws NotInDatabaseException;
+
+    /**
+     * Retrieves the product with the given barcode if it is stored in the database.
+     * The product does not have to be available in any store.
+     *
+     * @return The {@code ProductTO} with the given ID or null if it was not found
+     * @throws NotInDatabaseException
+     */
+    @WebMethod
+    ProductTO getProductByBarcode(
+            @XmlElement(required = true) @WebParam(name = "barcode") long barcode) throws NotInDatabaseException;
+
+    @WebMethod
+    long createProduct(
+            @XmlElement(required = true) @WebParam(name = "productTO") ProductTO productTO)
+            throws CreateException;
+
+    @WebMethod
+    void updateProduct(
+            @XmlElement(required = true) @WebParam(name = "productTO") ProductWithSupplierTO productTO)
+            throws UpdateException, NotInDatabaseException;
+
+    /* CRUD for {@link SupplierTO} **************/
 
     /**
      * Retrieves the supplier with the given ID if it is stored in the database.
@@ -246,35 +241,48 @@ public interface IEnterpriseManager {
             @XmlElement(required = true) @WebParam(name = "enterpriseID") long enterpriseID,
             @XmlElement(required = true) @WebParam(name = "productBarcode") long productBarcode) throws NotInDatabaseException;
 
+    /* CRUD for {@link StoreTO} **************/
+
     /**
-     * Retrieves the product with the given barcode if it is stored in the database.
-     * The product does not have to be available in any store.
-     *
-     * @return The {@code ProductTO} with the given ID or null if it was not found
-     * @throws NotInDatabaseException
+     * @param enterpriseId the unique identifier of a TradingEnterprise entity
+     * @return A collection of StoreWithEntepriseTO objects belonging to the given enterprise.
+     * @throws NotInDatabaseException if a trading enterprise with the given id could not be found
      */
     @WebMethod
-    ProductTO getProductByBarcode(
-            @XmlElement(required = true) @WebParam(name = "barcode") long barcode) throws NotInDatabaseException;
+    Collection<StoreWithEnterpriseTO> queryStoresByEnterpriseID(
+            @XmlElement(required = true) @WebParam(name = "enterpriseID") long enterpriseId) throws NotInDatabaseException;
 
+    /**
+     * @param enterpriseId the unique identifier of a TradingEnterprise entity
+     * @param storeId      the unique identifier of a Store entity
+     * @return A StoreWithEntepriseTO object with the given store identifier and
+     * belonging to the given enterprise.
+     * @throws NotInDatabaseException if a trading enterprise with the given id could not be found
+     */
     @WebMethod
-    long createEnterprise(
-            @XmlElement(required = true) @WebParam(name = "enterpriseName") String enterpriseName)
-            throws CreateException;
+    StoreWithEnterpriseTO queryStoreByEnterpriseID(
+            @XmlElement(required = true) @WebParam(name = "enterpriseID") long enterpriseId,
+            @XmlElement(required = true) @WebParam(name = "storeID") long storeId) throws NotInDatabaseException;
+
+    /**
+     * Queries the database for a store with the given name in the given enterprise.
+     * If there are multiple stores with the given name, all of them are returned.
+     * If there is no store with the given name, an empty collection is returned.
+     *
+     * @param enterpriseId the unique identifier of a TradingEnterprise entity
+     * @param storeName    the name of a Store entity
+     * @return All StoreWithEntepriseTO objects matching the given store name and
+     * belonging to the given enterprise.
+     * @throws NotInDatabaseException if a trading enterprise with the given id could not be found
+     */
+    @WebMethod
+    Collection<StoreWithEnterpriseTO> queryStoreByName(
+            @XmlElement(required = true) @WebParam(name = "enterpriseID") long enterpriseId,
+            @XmlElement(required = true) @WebParam(name = "storeName") String storeName) throws NotInDatabaseException;
 
     @WebMethod
     long createStore(
             @XmlElement(required = true) @WebParam(name = "storeTO") StoreWithEnterpriseTO storeTO)
-            throws CreateException;
-
-    @WebMethod
-    long createPlant(
-            @XmlElement(required = true) @WebParam(name = "plantTO") PlantTO plantTO)
-            throws CreateException;
-
-    @WebMethod
-    long createCustomProduct(
-            @XmlElement(required = true) @WebParam(name = "customProductTO") CustomProductTO customProductTO)
             throws CreateException;
 
     /**
@@ -290,28 +298,36 @@ public interface IEnterpriseManager {
             @XmlElement(required = true) @WebParam(name = "storeTO") StoreWithEnterpriseTO storeTO)
             throws UpdateException, NotInDatabaseException;
 
+    /* CRUD for {@link CustomProductTO} **************/
+
     /**
-     * Updates the plant object. This method requires the EnterpriseTO to be present and to have
-     * at least the id attribute set.
+     * Retrieves all products that are sold in this enterprise.
+     * Note that there is no information included about the stores in which
+     * this product is available.
      *
-     * @param plantTO
-     * @throws UpdateException
+     * @param enterpriseId The enterprise for which all products should be retrieved
+     * @return All {@code ProductTO}s available in the given enterprise
      * @throws NotInDatabaseException
      */
     @WebMethod
-    void updatePlant(
-            @XmlElement(required = true) @WebParam(name = "plantTO") PlantTO plantTO)
-            throws UpdateException, NotInDatabaseException;
+    Collection<CustomProductTO> getAllEnterpriseCustomProducts(
+            @XmlElement(required = true) @WebParam(name = "enterpriseID") long enterpriseId)
+            throws NotInDatabaseException;
+
+    /**
+     * Retrieves all products that are registered in the database.
+     * Note that there is no information included about the stores in which
+     * these products are available.
+     *
+     * @return All {@code ProductTO}s available or an empty collection if there are none
+     */
+    @WebMethod
+    Collection<CustomProductTO> getAllCustomProducts();
 
     @WebMethod
-    long createProduct(
-            @XmlElement(required = true) @WebParam(name = "productTO") ProductTO productTO)
+    long createCustomProduct(
+            @XmlElement(required = true) @WebParam(name = "customProductTO") CustomProductTO customProductTO)
             throws CreateException;
-
-    @WebMethod
-    void updateProduct(
-            @XmlElement(required = true) @WebParam(name = "productTO") ProductWithSupplierTO productTO)
-            throws UpdateException, NotInDatabaseException;
 
     @WebMethod
     void updateCustomProduct(
@@ -319,22 +335,26 @@ public interface IEnterpriseManager {
             throws UpdateException, NotInDatabaseException;
 
     @WebMethod
-    Collection<EnterpriseTO> getEnterprises();
-
-    @WebMethod
-    void deleteEnterprise(
-            @XmlElement(required = true) @WebParam(name = "enterpriseTO") EnterpriseTO enterpriseTO)
-            throws NotInDatabaseException, UpdateException, IOException;
-
-    @WebMethod
-    void deletePlant(
-            @XmlElement(required = true)
-            @WebParam(name = "plantTO") PlantTO plantTO)
-            throws NotInDatabaseException, UpdateException, IOException;
-
-    @WebMethod
     void deleteCustomProduct(
             @XmlElement(required = true)
             @WebParam(name = "customProductTO") CustomProductTO customProductTO)
             throws NotInDatabaseException, UpdateException, IOException;
+
+    /* CRUD for {@link ProductionUnitClassTO} **************/
+
+
+
+    /* Other Methods **************/
+
+    /**
+     * @param supplier   The supplier which delivers the products
+     * @param enterprise The enterprise for which the products are delivered
+     * @return The mean time to delivery in milliseconds
+     * @throws NotInDatabaseException
+     */
+    @WebMethod
+    long getMeanTimeToDelivery(
+            @XmlElement(required = true) @WebParam(name = "productSupplier") SupplierTO supplier,
+            @XmlElement(required = true) @WebParam(name = "enterprise") EnterpriseTO enterprise) throws NotInDatabaseException;
+
 }
