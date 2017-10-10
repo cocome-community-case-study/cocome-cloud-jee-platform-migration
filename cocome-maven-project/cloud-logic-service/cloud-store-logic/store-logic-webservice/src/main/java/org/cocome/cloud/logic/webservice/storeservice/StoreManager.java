b42354ce64dcbd162729a5129ee68ed917fe8055
Rudolf Biczok <rcbiczok@gmail.com>
2017-10-10 10:17:27 +0200

    Fix warnings & formatting

--- cocome-maven-project/cloud-logic-service/cloud-store-logic/store-logic-webservice/src/main/java/org/cocome/cloud/logic/webservice/storeservice/StoreManager.java
+++ cocome-maven-project/cloud-logic-service/cloud-store-logic/store-logic-webservice/src/main/java/org/cocome/cloud/logic/webservice/storeservice/StoreManager.java
@@ -1,33 +1,10 @@
 package org.cocome.cloud.logic.webservice.storeservice;
 
-import java.net.URISyntaxException;
-import java.util.List;
-
-import javax.ejb.CreateException;
-import javax.ejb.Stateless;
-import javax.inject.Inject;
-import javax.jws.WebService;
-
 import org.apache.log4j.Logger;
 import org.cocome.cloud.logic.registry.client.IApplicationHelper;
 import org.cocome.cloud.registry.service.Names;
 import org.cocome.logic.webservice.storeservice.IStoreManager;
-import org.cocome.tradingsystem.inventory.application.store.ComplexOrderEntryTO;
-import org.cocome.tradingsystem.inventory.application.store.ComplexOrderTO;
-import org.cocome.tradingsystem.inventory.application.store.IStoreInventoryLocal;
-import org.cocome.tradingsystem.inventory.application.store.IStoreInventoryManagerLocal;
-import org.cocome.tradingsystem.inventory.application.store.InvalidRollInRequestException;
-import org.cocome.tradingsystem.inventory.application.store.NoSuchProductException;
-import org.cocome.tradingsystem.inventory.application.store.NotImplementedException;
-import org.cocome.tradingsystem.inventory.application.store.ProductMovementTO;
-import org.cocome.tradingsystem.inventory.application.store.ProductOutOfStockException;
-import org.cocome.tradingsystem.inventory.application.store.ProductTO;
-import org.cocome.tradingsystem.inventory.application.store.ProductWithStockItemTO;
-import org.cocome.tradingsystem.inventory.application.store.ProductWithSupplierAndStockItemTO;
-import org.cocome.tradingsystem.inventory.application.store.ProductWithSupplierTO;
-import org.cocome.tradingsystem.inventory.application.store.SaleTO;
-import org.cocome.tradingsystem.inventory.application.store.StockItemTO;
-import org.cocome.tradingsystem.inventory.application.store.StoreWithEnterpriseTO;
+import org.cocome.tradingsystem.inventory.application.store.*;
 import org.cocome.tradingsystem.inventory.data.enterprise.IEnterpriseQuery;
 import org.cocome.tradingsystem.inventory.data.persistence.IPersistenceContext;
 import org.cocome.tradingsystem.inventory.data.persistence.UpdateException;
@@ -40,169 +17,177 @@ import org.cocome.tradingsystem.util.scope.ICashDeskRegistryFactory;
 import org.cocome.tradingsystem.util.scope.IContextRegistry;
 import org.cocome.tradingsystem.util.scope.RegistryKeys;
 
-@WebService(serviceName = "IStoreManagerService", 
-			name = "IStoreManager", 
-			endpointInterface = "org.cocome.logic.webservice.storeservice.IStoreManager",
-			targetNamespace = "http://store.webservice.logic.cocome.org/")
+import javax.ejb.CreateException;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.jws.WebService;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@WebService(serviceName = "IStoreManagerService",
+        name = "IStoreManager",
+        endpointInterface = "org.cocome.logic.webservice.storeservice.IStoreManager",
+        targetNamespace = "http://store.webservice.logic.cocome.org/")
 @Stateless
 public class StoreManager implements IStoreManager {
-	@Inject
-	IStoreInventoryManagerLocal storeManager;
-	
-	@Inject
-	IStoreInventoryLocal storeInventory;
-	
-	@Inject
-	ICashDeskRegistryFactory contextFactory;
-	
-	@Inject
-	IStoreQuery storeQuery;
-	
-	@Inject
-	IEnterpriseQuery enterpriseQuery;
-	
-	@Inject
-	IPersistenceContext persistenceContext;
-	
-	@Inject
-	IApplicationHelper applicationHelper;
-	
-	@Inject
-	IStoreDataFactory storeFactory;
-	
-	@Inject
-	String storeManagerWSDL;
-	
-	@Inject
-	long defaultStoreIndex;
-	
-	private static final Logger LOG = Logger.getLogger(StoreManager.class);
-	
-	private void setContextRegistry(long storeID) throws NotInDatabaseException {
-		LOG.debug("Setting store to store with id " + storeID);
-		IStore store = storeQuery.queryStoreById(storeID);
-		long enterpriseID = store.getEnterprise().getId();
-		
-		IContextRegistry registry = new CashDeskRegistry("store#" + storeID);
-		registry.putLong(RegistryKeys.STORE_ID, storeID);
-		registry.putLong(RegistryKeys.ENTERPRISE_ID, enterpriseID);
-		
-		contextFactory.setStoreContext(registry);
-		
-		try {
-			applicationHelper.registerComponent(Names.getStoreManagerRegistryName(defaultStoreIndex), storeManagerWSDL, false);
-			applicationHelper.registerComponent(Names.getStoreManagerRegistryName(storeID), storeManagerWSDL, false);
-		} catch (URISyntaxException e) {
-			LOG.error("Error registering component: " + e.getMessage());
-		}
-	}
-	
-	@Override
-	public void accountSale(long storeID, SaleTO sale) 
-			throws ProductOutOfStockException, NotInDatabaseException, UpdateException {
-		setContextRegistry(storeID);
-		storeManager.accountSale(storeID, sale);
-	}
 
-	@Override
-	public StoreWithEnterpriseTO getStore(long storeID) throws NotInDatabaseException {
-		setContextRegistry(storeID);
-		return storeManager.getStore(storeID);
-	}
+    @Inject
+    private IStoreInventoryManagerLocal storeManager;
 
-	@Override
-	public List<ProductWithStockItemTO> getProductsWithLowStock(long storeID) 
-			throws NotInDatabaseException {
-		setContextRegistry(storeID);
-		return storeManager.getProductsWithLowStock(storeID);
+    @Inject
+    private IStoreInventoryLocal storeInventory;
 
-	}
+    @Inject
+    private ICashDeskRegistryFactory contextFactory;
 
-	@Override
-	public List<ProductWithSupplierTO> getAllStoreProducts(long storeID) 
-			throws NotInDatabaseException {
-		setContextRegistry(storeID);
-		return storeManager.getAllProducts(storeID);
-	}
+    @Inject
+    private IStoreQuery storeQuery;
 
-	@Override
-	public List<ProductWithSupplierAndStockItemTO> getProductsWithStockItems(long storeID) 
-			throws NotInDatabaseException {
-		setContextRegistry(storeID);
-		return storeManager.getProductsWithStockItems(storeID);
-	}
+    @Inject
+    private IEnterpriseQuery enterpriseQuery;
 
-	@Override
-	public List<ComplexOrderTO> orderProducts(long storeID, ComplexOrderTO complexOrder) 
-			throws NotInDatabaseException, CreateException, UpdateException {
-		setContextRegistry(storeID);
-		return storeManager.orderProducts(storeID, complexOrder);
-	}
+    @Inject
+    private IPersistenceContext persistenceContext;
 
-	@Override
-	public ComplexOrderTO getOrder(long storeID, long orderId) 
-			throws NotInDatabaseException {
-		setContextRegistry(storeID);
-		return storeManager.getOrder(storeID, orderId);
+    @Inject
+    private IApplicationHelper applicationHelper;
 
-	}
+    @Inject
+    private IStoreDataFactory storeFactory;
 
-	@Override
-	public List<ComplexOrderTO> getOutstandingOrders(long storeID) 
-			throws NotInDatabaseException {
-		setContextRegistry(storeID);
-		return storeManager.getOutstandingOrders(storeID);
-	}
+    @Inject
+    private String storeManagerWSDL;
 
-	@Override
-	public void rollInReceivedOrder(long storeID, long orderId)
-			throws InvalidRollInRequestException, NotInDatabaseException, UpdateException {
-		setContextRegistry(storeID);
-		storeManager.rollInReceivedOrder(storeID, orderId);
-	}
+    @Inject
+    private long defaultStoreIndex;
 
-	@Override
-	public ProductWithStockItemTO changePrice(long storeID, StockItemTO stockItemTO) 
-			throws NotInDatabaseException, UpdateException {
-		LOG.debug("Changing price from stockItem " + stockItemTO.getId() + " to " + stockItemTO.getSalesPrice());
-		setContextRegistry(storeID);
-		return storeManager.changePrice(storeID, stockItemTO);
-	}
+    private static final Logger LOG = Logger.getLogger(StoreManager.class);
 
-	@Override
-	public void markProductsUnavailableInStock(long storeID, ProductMovementTO movedProductAmounts)
-			throws ProductOutOfStockException, NotInDatabaseException, UpdateException {
-		setContextRegistry(storeID);
-		storeManager.markProductsUnavailableInStock(storeID, movedProductAmounts);
+    private void setContextRegistry(long storeID) throws NotInDatabaseException {
+        LOG.debug("Setting store to store with id " + storeID);
+        IStore store = storeQuery.queryStoreById(storeID);
+        long enterpriseID = store.getEnterprise().getId();
+
+        IContextRegistry registry = new CashDeskRegistry("store#" + storeID);
+        registry.putLong(RegistryKeys.STORE_ID, storeID);
+        registry.putLong(RegistryKeys.ENTERPRISE_ID, enterpriseID);
+
+        contextFactory.setStoreContext(registry);
+
+        try {
+            applicationHelper.registerComponent(Names.getStoreManagerRegistryName(defaultStoreIndex), storeManagerWSDL, false);
+            applicationHelper.registerComponent(Names.getStoreManagerRegistryName(storeID), storeManagerWSDL, false);
+        } catch (URISyntaxException e) {
+            LOG.error("Error registering component: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void accountSale(long storeID, SaleTO sale)
+            throws ProductOutOfStockException, NotInDatabaseException, UpdateException {
+        setContextRegistry(storeID);
+        storeManager.accountSale(storeID, sale);
+    }
+
+    @Override
+    public StoreWithEnterpriseTO getStore(long storeID) throws NotInDatabaseException {
+        setContextRegistry(storeID);
+        return storeManager.getStore(storeID);
+    }
+
+    @Override
+    public List<ProductWithStockItemTO> getProductsWithLowStock(long storeID)
+            throws NotInDatabaseException {
+        setContextRegistry(storeID);
+        return storeManager.getProductsWithLowStock(storeID);
+
+    }
+
+    @Override
+    public List<ProductWithSupplierTO> getAllStoreProducts(long storeID)
+            throws NotInDatabaseException {
+        setContextRegistry(storeID);
+        return storeManager.getAllProducts(storeID);
+    }
+
+    @Override
+    public List<ProductWithSupplierAndStockItemTO> getProductsWithStockItems(long storeID)
+            throws NotInDatabaseException {
+        setContextRegistry(storeID);
+        return storeManager.getProductsWithStockItems(storeID);
+    }
+
+    @Override
+    public List<ComplexOrderTO> orderProducts(long storeID, ComplexOrderTO complexOrder)
+            throws NotInDatabaseException, CreateException, UpdateException {
+        setContextRegistry(storeID);
+        return storeManager.orderProducts(storeID, complexOrder);
+    }
+
+    @Override
+    public ComplexOrderTO getOrder(long storeID, long orderId)
+            throws NotInDatabaseException {
+        setContextRegistry(storeID);
+        return storeManager.getOrder(storeID, orderId);
+
+    }
+
+    @Override
+    public List<ComplexOrderTO> getOutstandingOrders(long storeID)
+            throws NotInDatabaseException {
+        setContextRegistry(storeID);
+        return storeManager.getOutstandingOrders(storeID);
+    }
+
+    @Override
+    public void rollInReceivedOrder(long storeID, long orderId)
+            throws InvalidRollInRequestException, NotInDatabaseException, UpdateException {
+        setContextRegistry(storeID);
+        storeManager.rollInReceivedOrder(storeID, orderId);
+    }
+
+    @Override
+    public ProductWithStockItemTO changePrice(long storeID, StockItemTO stockItemTO)
+            throws NotInDatabaseException, UpdateException {
+        LOG.debug("Changing price from stockItem " + stockItemTO.getId() + " to " + stockItemTO.getSalesPrice());
+        setContextRegistry(storeID);
+        return storeManager.changePrice(storeID, stockItemTO);
+    }
+
+    @Override
+    public void markProductsUnavailableInStock(long storeID, ProductMovementTO movedProductAmounts)
+            throws ProductOutOfStockException, NotInDatabaseException, UpdateException {
+        setContextRegistry(storeID);
+        storeManager.markProductsUnavailableInStock(storeID, movedProductAmounts);
 
 
-	}
+    }
 
-	@Override
-	public ComplexOrderEntryTO[] getStockItems(long storeID, ProductTO[] requiredProductTOs) 
-			throws NotInDatabaseException, NotImplementedException {
-		setContextRegistry(storeID);
-		return storeManager.getStockItems(storeID, requiredProductTOs);
+    @Override
+    public ComplexOrderEntryTO[] getStockItems(long storeID, ProductTO[] requiredProductTOs)
+            throws NotInDatabaseException, NotImplementedException {
+        setContextRegistry(storeID);
+        return storeManager.getStockItems(storeID, requiredProductTOs);
 
-	}
+    }
 
-	@Override
-	public ProductWithStockItemTO getProductWithStockItem(long storeID, long productBarcode) 
-			throws NoSuchProductException, NotInDatabaseException {
-		setContextRegistry(storeID);
-		return storeInventory.getProductWithStockItem(storeID, productBarcode);
-	}
+    @Override
+    public ProductWithStockItemTO getProductWithStockItem(long storeID, long productBarcode)
+            throws NoSuchProductException, NotInDatabaseException {
+        setContextRegistry(storeID);
+        return storeInventory.getProductWithStockItem(storeID, productBarcode);
+    }
 
-	@Override
-	public void updateStockItem(long storeID, StockItemTO stockItemTO) throws NotInDatabaseException, UpdateException {
-		setContextRegistry(storeID);
-		storeManager.updateStockItem(storeID, stockItemTO);
-	}
+    @Override
+    public void updateStockItem(long storeID, StockItemTO stockItemTO) throws NotInDatabaseException, UpdateException {
+        setContextRegistry(storeID);
+        storeManager.updateStockItem(storeID, stockItemTO);
+    }
 
-	@Override
-	public void createStockItem(long storeID, ProductWithStockItemTO stockItemTO)
-			throws NotInDatabaseException, CreateException {
-		setContextRegistry(storeID);
-		storeManager.createStockItem(storeID, stockItemTO);
-	}
+    @Override
+    public void createStockItem(long storeID, ProductWithStockItemTO stockItemTO)
+            throws NotInDatabaseException, CreateException {
+        setContextRegistry(storeID);
+        storeManager.createStockItem(storeID, stockItemTO);
+    }
 }
