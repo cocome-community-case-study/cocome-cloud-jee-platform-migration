60363d2f787640476226d06e1350254312d041a0
Rudolf Biczok <rcbiczok@gmail.com>
2017-11-21 11:39:44 +0100

    Move utility classes to API modules to make them visible to other web service unit tests

---
+++ cocome-maven-project/cloud-logic-service/cloud-logic-core-api/src/main/java/org/cocome/tradingsystem/inventory/application/plant/iface/ppu/doub/XPPU.java
@@ -0,0 +1,50 @@
+package org.cocome.tradingsystem.inventory.application.plant.iface.ppu.doub;
+
+import org.cocome.tradingsystem.inventory.application.plant.iface.ppu.PUCOperationMeta;
+
+/**
+ * eXtended Pick & Place Unit
+ *
+ * @author Rudolf Biczok
+ */
+public enum XPPU implements PUCOperationMeta {
+    ACT_DriveFromBaseToRamp1("_1_2_1_P1_O3", 2),
+    ACT_DriveFromBaseToRamp2("_1_2_1_P1_O5", 2),
+    ACT_DriveFromBaseToEnd("_1_2_1_P1_O6", 2),
+    ACT_PushToRamp1("_1_2_1_P1_O7", 2),
+    ACT_PushToRamp2("_1_2_1_P1_O8", 2),
+    Crane_ACT_Init("_1_2_1_P2_O1", 1),
+    Crane_ACT_PickUpWP("_1_2_1_P2_O2", 5),
+    Crane_ACT_PutDownWP("_1_2_1_P2_O3", 5),
+    Crane_ACT_TurnToConveyor("_1_2_1_P2_O4", 5),
+    Crane_ACT_TurnToStack("_1_2_1_P2_O5", 5),
+    Crane_ACT_TurnToStamp("_1_2_1_P2_O6", 5),
+    Stamp_ACT_Init("_1_2_1_P3_O1", 1),
+    Stamp_ACT_Stamp("_1_2_1_P3_O2", 1),
+    Stack_ACT_Init("_1_2_1_P4_O1", 1),
+    Stack_ACT_ProvideWP("_1_2_1_P4_O2", 2);
+
+    private String operationId;
+    private long execTimeInMillis;
+
+    XPPU(final String operationId, final long execTimeInMillis) {
+        this.operationId = operationId;
+        this.execTimeInMillis = execTimeInMillis;
+    }
+
+    @Override
+    public String getOperationId() {
+        return operationId;
+    }
+
+    @Override
+    public String getName() {
+        return this.name();
+    }
+
+    @Override
+    public long getExecutionDurationInMillis() {
+        return this.execTimeInMillis;
+    }
+
+}
---
+++ cocome-maven-project/cloud-logic-service/cloud-logic-core-services/src/main/java/org/cocome/tradingsystem/inventory/application/plant/iface/PUCImporter.java
@@ -0,0 +1,63 @@
+package org.cocome.tradingsystem.inventory.application.plant.iface;
+
+import org.cocome.cloud.logic.stub.CreateException_Exception;
+import org.cocome.cloud.logic.stub.IPlantManager;
+import org.cocome.tradingsystem.inventory.application.plant.PlantTO;
+import org.cocome.tradingsystem.inventory.application.plant.iface.ppu.PUCOperationMeta;
+import org.cocome.tradingsystem.inventory.application.plant.productionunit.ProductionUnitClassTO;
+import org.cocome.tradingsystem.inventory.application.plant.productionunit.ProductionUnitOperationTO;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Test class, just for alleviating the work with plant-specific data structures in test cases
+ *
+ * @author Rudolf Biczok
+ */
+public class PUCImporter {
+
+    private ProductionUnitClassTO puc;
+
+    private Map<PUCOperationMeta, ProductionUnitOperationTO> operators;
+
+    public PUCImporter(final String pucName,
+                       final PUCOperationMeta[] operations,
+                       final PlantTO plant,
+                       final IPlantManager pm) throws CreateException_Exception {
+
+        puc = new ProductionUnitClassTO();
+        puc.setName(pucName);
+        puc.setPlant(plant);
+        puc.setId(pm.createProductionUnitClass(puc));
+
+        operators = Arrays.stream(operations).collect(Collectors.toMap(Function.identity(),
+                e -> {
+                    final ProductionUnitOperationTO operation = new ProductionUnitOperationTO();
+                    operation.setName(e.getName());
+                    operation.setOperationId(e.getOperationId());
+                    operation.setExecutionDurationInMillis(e.getExecutionDurationInMillis());
+                    operation.setProductionUnitClass(puc);
+                    try {
+                        operation.setId(pm.createProductionUnitOperation(
+                                operation));
+                    } catch (CreateException_Exception e1) {
+                        throw new IllegalArgumentException("Cannot create production unit operation: "
+                                + e1.getMessage(),
+                                e1);
+                    }
+                    return operation;
+                }));
+    }
+
+    public ProductionUnitClassTO getProductionUnitClass() {
+        return puc;
+    }
+
+    public ProductionUnitOperationTO getOperation(final PUCOperationMeta op) {
+        return this.operators.get(op);
+    }
+
+}
